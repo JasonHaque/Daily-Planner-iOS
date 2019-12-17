9d2a36ef10f6b165f4087b120b510e6693eceb8f
JasonHaque <zahinjason220434@gmail.com>
2019-12-17 20:36:35 +0600

    added new IB outlets and actions

--- DailyPlanner/ViewControllers/LoginViewController.swift
+++ DailyPlanner/ViewControllers/LoginViewController.swift
@@ -10,6 +10,9 @@ import UIKit
 
 class LoginViewController: UIViewController {
 
+    @IBOutlet weak var LoginMail: UITextField!
+    
+    @IBOutlet weak var LoginPassword: UITextField!
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -17,14 +20,8 @@ class LoginViewController: UIViewController {
     }
     
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    
+    @IBAction func LoginTapped(_ sender: Any) {
     }
-    */
-
+    
 }
