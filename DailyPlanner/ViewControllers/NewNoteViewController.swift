1e51d16bcaae046e7e594084861d980ce2d06106
JasonHaque <zahinjason220434@gmail.com>
2020-01-02 23:25:25 +0600

    added ib action listeners

--- DailyPlanner/ViewControllers/NewNoteViewController.swift
+++ DailyPlanner/ViewControllers/NewNoteViewController.swift
@@ -10,21 +10,22 @@ import UIKit
 
 class NewNoteViewController: UIViewController {
 
+    @IBOutlet weak var NoteName: UITextField!
+    
+    @IBOutlet weak var NoteDesc: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @IBAction func SaveButtonTapped(_ sender: Any) {
     }
-    */
-
+    
+    @IBAction func CancelButtonTapped(_ sender: Any) {
+    }
+    
+    @IBAction func ViewButtonTapped(_ sender: Any) {
+    }
+    
 }
