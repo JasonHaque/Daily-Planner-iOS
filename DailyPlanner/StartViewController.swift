c6a2c349282d3082317c10547e7fae3511ba3722
JasonHaque <zahinjason220434@gmail.com>
2020-05-08 20:46:35 +0600

    code cleaned

---
+++ DailyPlanner/StartViewController.swift
@@ -0,0 +1,22 @@
+//
+//  StartViewController.swift
+//  DailyPlanner
+//
+//  Created by Sanviraj Zahin Haque on 8/5/20.
+//  Copyright © 2020 Sanviraj Zahin Haque. All rights reserved.
+//
+
+import UIKit
+
+class StartViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    
+
+}
