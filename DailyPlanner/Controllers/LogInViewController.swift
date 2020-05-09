6ebe2ae56b1dd8ce0990e2cca137fbcbf3cea240
JasonHaque <zahinjason220434@gmail.com>
2020-05-09 14:46:26 +0600

    ib outlets added

--- DailyPlanner/Controllers/LogInViewController.swift
+++ DailyPlanner/Controllers/LogInViewController.swift
@@ -9,22 +9,20 @@
 import UIKit
 
 class LogInViewController: UIViewController {
-
+    //IB connection outletds
+    @IBOutlet weak var EmailAddressField: UITextField!
+    @IBOutlet weak var PasswordField: UITextField!
+    @IBOutlet weak var LoginLabel: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        setupViews()
+    }
+    
+    func setupViews(){
+        LoginLabel.alpha = 0
     }
     
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
+    
 
 }
--- DailyPlanner/Controllers/SignUpViewController.swift
+++ DailyPlanner/Controllers/SignUpViewController.swift
@@ -10,21 +10,22 @@ import UIKit
 
 class SignUpViewController: UIViewController {
 
+    //IB outlet labels
+    @IBOutlet weak var EmailAddressField: UITextField!
+    @IBOutlet weak var PasswordField: UITextField!
+    @IBOutlet weak var ConfirmPassword: UITextField!
+    @IBOutlet weak var SignUpLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        setupViews()
+    }
+    
+    func setupViews(){
+        SignUpLabel.alpha = 0
     }
     
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
+    
 
 }
